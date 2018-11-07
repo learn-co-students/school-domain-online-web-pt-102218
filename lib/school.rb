b073911153d580e69fc37b26480ad3b1a1a01021
Joshua D. Forrest <joshuadforrest@gmail.com>
2018-11-07 14:43:37 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
-# code here!
+class School
+  
+  roster={}
+  
+  def add_student(name, num)
+    if roster[:num] == nil
+      roster[:num] = []
+      roster[:num] << name
+    else
+      roster[:num] << name
+    end
+  end
+  
+  def grade(g_num)
+    roster[num]
+  end
+  
+  def sort
+    roster.each do |grade, names|
+      grade.sort
+    
+  end
+  
+end
