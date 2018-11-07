d06ca8f132b75abb836dbd0dc3523406bf8e68a2
Linda Chang <lindachang186@gmail.com>
2018-11-07 21:13:33 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
 # code here!
+require "pry"
+class School
+
+attr_accessor :roster
+
+def initialize(new)
+   @roster = {}
+   @school = new
+ end
+
+ def add_student(name, grade)
+    @roster[grade] = []
+     if @roster[grade]||= grade
+     @roster[grade] << name
+   end
+ end
+
+
+ def grade(grades)
+   @roster[grades]
+ end
+
+ def sort
+   @roster.map do |key, value|
+  end
+    return @roster
+ end
+
+ end
