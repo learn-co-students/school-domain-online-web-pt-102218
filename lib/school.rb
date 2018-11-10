ce7345bf63cf066cdd05bd0348861a2c39d61a21
grant smith <grantsmith1256@gmail.com>
2018-11-10 04:36:03 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,23 +1,23 @@
 class School
   
   attr_accessor :student
-  attr_reader :roster
+  attr_reader :roster, :name
   
   def initialize(name)
     @name = name
+    @roster = {}
   end
   
-  def roster
-    hash = {}
-  end
   
   def add_student(grade, name)
-    school_hash = {}
-    school_hash.each do |grade, name|
-     school_hash.include?(grade) 
-      school_hash[grade] = [name]
-    end
-  end
+     if @roster.include?(grade)
+       @roster[grade] << name
+       else @roster[grade] = []
+       @roster[grade] = [name]
+     end
+   @roster
+ end
+
   
   def grade(grade)
   end
@@ -27,5 +27,4 @@ class School
       name.sort a<=>b
     end
   end
-
 end
